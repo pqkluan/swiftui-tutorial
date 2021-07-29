f676ff5c600af2e26f9624929541f242370ffce3
Luan Pham <pqkluan@gmail.com>
2021-07-29 12:12:26 +0700

    feat: animating views and transitions

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -10,6 +10,7 @@ import Foundation
 
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
 }
 
 /// Load file from main bundle, read it data and parse to the expected type.
